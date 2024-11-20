2b7aac1c1d1d9c5ca4129751a198c4aa5937f491
Chirag <chiragk927@gmail.com>
2024-11-20 12:34:42 -0500

    removed port variable

--- langchain_movie_search.py
+++ langchain_movie_search.py
@@ -130,4 +130,6 @@ def gradio_interface():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("PORT", "5000"), debug=True)
+    # app.run(host="0.0.0.0", port=os.getenv("PORT", "5000"), debug=True)
+    app.run(host="0.0.0.0", debug=True)
+    
